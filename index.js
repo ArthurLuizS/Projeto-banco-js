10e1cafc7a84426c83e38f662f165e1e966f2d4e
Arthur Luiz <arthurluiz36@gmail.com>
2020-06-03 19:03:18 -0300

    teste de deposito e transferencia por metodos de classes

---
+++ index.js
@@ -0,0 +1,33 @@
+
+
+import {cliente} from "./Cliente.js"
+import {contaCorrente} from "./ContaCorrente.js"
+
+
+const cliente1 = new cliente();
+const cliente2 = new cliente();
+
+cliente1.nome = "Arthur"
+cliente1.cpf = "001122"
+
+cliente2.nome = "Rafael"
+cliente2.cpf = "002212"
+
+
+const contaRafa = new contaCorrente()
+contaRafa.cliente = cliente2
+contaRafa.depositar(1000)
+//console.log(contaRafa._saldo)
+contaRafa.agencia = "1232"
+
+console.log(contaRafa)
+
+const contaArthur = new contaCorrente();
+//console.log(`Arthur, ${contaArthur._saldo}`)
+contaArthur.agencia = 1411
+contaArthur.cliente = cliente1
+contaArthur.depositar(20)
+contaArthur.transferir(10, contaRafa)
+//contaRafa.transferir(200, contaArthur)
+console.log(contaRafa)
+console.log(contaArthur)
