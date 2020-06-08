9cb8ef60e49ae387b710bc451668192726f61d39
Arthur Luiz <arthurluiz36@gmail.com>
2020-06-08 18:20:04 -0300

    Manipulação de instancias e acionamento de metodos

--- index.js
+++ index.js
@@ -2,16 +2,16 @@
 
 import {cliente} from "./Cliente.js"
 import {contaCorrente} from "./ContaCorrente.js"
-import { ContaPoupança } from "./ContaPoupanca.js/index.js";
+import { ContaPoupança } from "./ContaPoupanca.js";
+import { ContaSalario } from "./ContaSalario.js";
+
 
 
 const cliente1 = new cliente("Arthur", 1122);
 
 const contaArthur = new contaCorrente(cliente1, 1411);
-contaArthur.depositar(4000)
-contaArthur.sacar(100)
-console.log(contaArthur)
-
-const contapoupança = new ContaPoupança(50, cliente1, 1411)
-contapoupança.sacar(10)
-console.log(contapoupança)
+const contapoupança = new ContaPoupança(50, cliente1, 1411);
+const ContSal = new ContaSalario(cliente1)
+ContSal.depositar(1000)
+ContSal.sacar(23)
+console.log(ContSal)
