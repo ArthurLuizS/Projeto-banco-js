f4f8373e9f24309e1b094e88b566f275396aa1d0
Arthur Luiz <arthurluiz36@gmail.com>
2020-06-09 18:48:15 -0300

    Manipulação das classes

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-
+import {Gerente} from "./Gerente.js"
+import {Diretor} from "./Diretor.js"
 
 import {cliente} from "./Cliente.js"
-import {contaCorrente} from "./ContaCorrente.js"
-import { ContaPoupança } from "./ContaPoupanca.js";
-import { ContaSalario } from "./ContaSalario.js";
+import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
 
 
+const GerenteA = new Gerente("Arthur", 12301230, 5000)
+GerenteA.CadastrarSenha(1230)
+const DiretorB = new Diretor ("Rafael", 12401240, 10000)
+const Cliente = new cliente("le", 1234321, "123")
 
-const cliente1 = new cliente("Arthur", 1122);
-
-const contaArthur = new contaCorrente(cliente1, 1411);
-const contapoupança = new ContaPoupança(50, cliente1, 1411);
-const ContSal = new ContaSalario(cliente1)
-ContSal.depositar(1000)
-ContSal.sacar(23)
-console.log(ContSal)
+const statusc = SistemaAutenticacao.login(Cliente, "123")
+const status = SistemaAutenticacao.login(GerenteA, "1230")
+console.log(status, statusc)
