f201166ab6a77c4507215b3e7fd4150a8f0f9579
Arthur Luiz <arthurluiz36@gmail.com>
2020-06-09 18:43:06 -0300

    Classe abstrata

--- Conta.js
+++ Conta.js
@@ -1,53 +1,53 @@
 //Classe abstrata
 export class Conta {
-    constructor(saldoinicial, cliente, agencia){
-        if(this.constructor == Conta){
-            throw new Error ("Você não pode instanciar diretamente um objeto do tipo Conta");
+    constructor(saldoinicial, cliente, agencia) {
+        if (this.constructor == Conta) {
+            throw new Error("Você não pode instanciar diretamente um objeto do tipo Conta");
         }
         this._saldo = saldoinicial;
         this._cliente = cliente;
         this._agencia = agencia;
 
     }
-    set cliente (novoValor){
-        if (novoValor instanceof cliente){
+    set cliente(novoValor) {
+        if (novoValor instanceof cliente) {
             this._cliente = novoValor;
         }
 
     }
 
-    get cliente (){
+    get cliente() {
         return this._cliente;
     }
     //por convenção da comunidade, qualquer variavel ou atributo que tenha um _ como primeiro elemento
     //na sua declaração
     //deve ser tratada como uma variavel ou atributo privado, e nao deverá ser usado fora da classe
-    
-    get saldo(){
+
+    get saldo() {
         return this._saldo
     }
     //Metodo abstrato - precisa ser sobrescrito em cada classe filha 
-    sacar(valor){
-       throw new Error("O metodo chamado é abstrato")
+    sacar(valor) {
+        throw new Error("O metodo chamado é abstrato")
     }
-    _sacar(valor, taxa){
+    _sacar(valor, taxa) {
         const valorSacado = taxa * valor
-        if (this._saldo >= valorSacado){
+        if (this._saldo >= valorSacado) {
             this._saldo -= valorSacado;
             return valorSacado;
-        } else{
+        } else {
             console.log("Valor informado acima do saldo da conta")
         }
     }
-    depositar(valor){
-        if (valor <= 0){
+    depositar(valor) {
+        if (valor <= 0) {
             return; /*o return aqui, fara com que o comando "depositar" seja interrompido, 
                     semelhante ao comando "break" */
         }
         this._saldo += valor;
     }
 
-    transferir (valor, conta){
+    transferir(valor, conta) {
         const valorSacado = this.sacar(valor)
         conta.depositar(valorSacado)
 
