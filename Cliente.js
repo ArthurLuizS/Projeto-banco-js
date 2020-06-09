ae37c755108d8b024c253e93c806d69a0124488f
Arthur Luiz <arthurluiz36@gmail.com>
2020-06-09 18:42:20 -0300

    Classe cliente

--- Cliente.js
+++ Cliente.js
@@ -1,12 +1,18 @@
-export class cliente{
+export class cliente {
     get cpf() {
         return this._cpf
     }
 
-    constructor(nome, cpf){
+    constructor(nome, cpf, senha) {
         this.nome = nome;
         this._cpf = cpf
+        this._senha = senha
+        
+    }
+    autenticar(){
+        return true
     }
     
-   
+
+
 }
